ca7a817cbf50e5709a4854f9f7094f7989c8aeae
Mohamed <mo.esmael2019@gmail.com>
2023-06-09 11:56:00 +0300

    fix 3

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use App\Models\FavouriteProduct;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductDetailResource;
+use App\Models\SubCategory;
 
 class ProductController extends Controller
 {
@@ -29,7 +30,7 @@ class ProductController extends Controller
         $data= New ProductDetailResource($product);
 
 
-            return responseApi(200, "product details Found", $data);
+        return responseApi(200, "product details Found", $data);
     }
 
 
@@ -96,18 +97,23 @@ class ProductController extends Controller
         return responseApi(200, "All favourite products", $fav_products);
     }
 
-    public function addMedia(Request $request)
+    public function addMediaNew(Request $request)
     {
         $validator = validator($request->all(), [
             'image' => 'required|Image|mimes:jpeg,jpg,png,gif',//|max:10000',
+            'colorId'=>'required',
+            'productId'=>'required',
         ]);
 
         if ($validator->fails())return responseApi('false', $validator->errors()->first());
 
         $uploadedFile = $request->file('image');
-        
-        $this->productModel->addMedia($uploadedFile)
-                ->withCustomProperties(['color_id' => $request->colorId,'product_id' => $request->productId])->toMediaCollection('images');
+        $product=$this->productModel->find($request->productId);
+        if($product){
+            $product->addMedia($uploadedFile)
+                ->withCustomProperties(['color_id' => (int)$request->colorId])->toMediaCollection('images');
+        }
+
 
         return responseApi(200, 'image uploaded successfuly');
     }
